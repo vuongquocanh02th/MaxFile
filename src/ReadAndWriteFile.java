ffc6f4c32a75e42cd56ce5202fc43382debb9207
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-11-24 09:45:47 +0700

    max file

---
+++ src/ReadAndWriteFile.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+    public List<Integer> readFile(String filePath) {
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+        } catch (Exception e) {
+            System.err.println("File not found or content error!");
+        }
+        return numbers;
+    }
+
+    public void writeFile(String filePath, int max) {
+        try {
+            FileWriter fileWriter = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("Max = " + max);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            if (max < numbers.get(i)) {
+                max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("D:\\CodeGym\\Module2\\max-file\\src\\numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("D:\\CodeGym\\Module2\\max-file\\src\\result.txt", maxValue);
+    }
+}
